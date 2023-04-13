8d28ade85bfed21cbc7f2f13e0120f34e7100911
jclare93 <106985713+lemonFlavDaBest@users.noreply.github.com>
2023-04-13 10:09:48 -0700

    Create YulERC20v2.sol
    adding the file to write along with the video

---
+++ erc20/YulERC20v2.sol
@@ -0,0 +1,26 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.17;
+
+// Used in the `name()` function
+bytes32 constant nameLength = 0x0000000000000000000000000000000000000000000000000000000000000009;
+bytes32 constant nameData = 0x59756c20546f6b656e0000000000000000000000000000000000000000000000;
+
+// Used in the `symbol()` function
+bytes32 constant symbolLength = 0x0000000000000000000000000000000000000000000000000000000000000003;
+bytes32 constant symbolData = 0x59554c0000000000000000000000000000000000000000000000000000000000;
+
+// `bytes4(keccak256("InsufficientBalance()"))`
+bytes32 constant insufficientBalanceSelector = 0xf4d678b800000000000000000000000000000000000000000000000000000000;
+
+// `bytes4(keccak256("InsufficientAllowance(address,address)"))`
+bytes32 constant insufficientAllowanceSelector = 0xf180d8f900000000000000000000000000000000000000000000000000000000;
+
+error InsufficientBalance();
+error InsufficientAllowance(address owner, address spender);
+
+/// @title Yul ERC20
+/// @author <your name here>
+/// @notice For demo purposes ONLY.
+contract YulERC20v2 {
+
+}
